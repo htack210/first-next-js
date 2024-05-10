90c6d9bfab5f3f42a0f2a009296568c2572d1b3b
htack210 <glenn.hline@outlook.com>
2024-05-10 10:44:26 -0400

    Added button using "use client" to handle click event.

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+
+const ProductCard = () => {
+  return (
+    <div>
+      ProductCard
+      <button onClick={() => console.log("Click")}>Add to Cart</button>
+    </div>
+  );
+};
+
+export default ProductCard;
